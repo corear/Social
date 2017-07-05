ff1da294a45afc517c7b909fb3d659e75ae0aff3
Collin <c_orear@icloud.com>
2017-07-05 00:57:25 +0000

    Base site.

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   get '/groups' => 'pages#groups'
 
   get '/prayers' => 'pages#prayers'
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
