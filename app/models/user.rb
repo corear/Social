fc24925d2b786a0a62aa222a51c4647725c6f75b
Collin <c_orear@icloud.com>
2017-08-24 20:51:59 +0000

    Aesthetic updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ActiveRecord::Base
     has_many :comments, dependent: :destroy
     
     
-    has_attached_file :avatar, styles: { medium: "256x256>", thumb: "50x50#", small: "100x100#" }, default_url: "/blank.jpg"
+    has_attached_file :avatar, styles: { medium: "256x256#", thumb: "50x50#", small: "100x100#" }, default_url: "/blank.jpg"
     validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
   
     def follow(other)
