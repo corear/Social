75a01969ac83ccdb654a36324c852a524b284b36
Collin <c_orear@icloud.com>
2018-04-12 16:03:38 +0000

    Kewl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
   
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me, :display_name, :description) }
-    devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me, :display_name) }
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :current_password, :password_confirmation, :remember_me, :avatar, :display_name, :username, :description) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me, :display_name, :description, :number) }
+    devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:email, :password, :password_confirmation, :remember_me) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :current_password, :password_confirmation, :remember_me, :avatar, :display_name, :username, :description, :number) }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170804153202) do
+ActiveRecord::Schema.define(version: 20180412140340) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -67,6 +67,8 @@ ActiveRecord::Schema.define(version: 20170804153202) do
     t.datetime "avatar_updated_at"
     t.string   "display_name"
     t.text     "description"
+    t.string   "name"
+    t.string   "number"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
